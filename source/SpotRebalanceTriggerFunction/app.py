5ddde189c978a95bc49dc780781de68f9324e77d
Scott Horsfield <horsfieldsa@gmail.com>
2020-11-18 17:03:33 -0800

    Adds handling of Rebalance Recommendation and adds sample Athena Query to see Rebalance Recommendations by ASG an time between Rebalance and Termination

---
+++ source/SpotRebalanceTriggerFunction/app.py
@@ -0,0 +1,70 @@
+ # Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ #
+ # Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ # software and associated documentation files (the "Software"), to deal in the Software
+ # without restriction, including without limitation the rights to use, copy, modify,
+ # merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ # permit persons to whom the Software is furnished to do so.
+ #
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ # INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ # PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ # HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import boto3
+import os
+import json
+
+from botocore.exceptions import ClientError
+
+instance_metadata_table = boto3.resource('dynamodb').Table(os.environ['INSTANCE_METADATA_TABLE'])
+
+def lambda_handler(event, context):
+
+    print(event)
+
+    # Transform CloudWatch Event
+    item = {
+        'InstanceId': event['detail']['instance-id'],
+        'Region': event['region'],
+        'LastEventTime': event['time'],
+        'LastEventType': 'rebalance-recommendation',
+        'RebalanceRecommended': True,
+        'RebalanceRecommendationTime': event['time']
+    }
+
+    print(item)
+
+    # Commit to DynamoDB
+    try:
+        response=instance_metadata_table.update_item(
+            Key={
+                'InstanceId': item['InstanceId']
+            },
+            UpdateExpression="SET #Region = :Region, #LastEventTime = :LastEventTime, #LastEventType = :LastEventType, #RebalanceRecommended = :RebalanceRecommended, #RebalanceRecommendationTime = :RebalanceRecommendationTime",
+            ExpressionAttributeNames={
+                '#Region' : 'Region',
+                '#LastEventTime' : 'LastEventTime',
+                '#LastEventType' : 'LastEventType',
+                '#RebalanceRecommended' : 'RebalanceRecommended',
+                '#RebalanceRecommendationTime' : 'RebalanceRecommendationTime'
+            },
+            ExpressionAttributeValues={
+                ':Region': item['Region'],
+                ':LastEventTime': item['LastEventTime'],
+                ':LastEventType': item['LastEventType'],
+                ':RebalanceRecommended': item['RebalanceRecommended'],
+                ':RebalanceRecommendationTime': item['RebalanceRecommendationTime']
+                },
+            ReturnValues="NONE"
+        )
+
+        print(response)
+    except ClientError as e:
+        print(e)
+
+    # End
+    print('Execution Complete')
+    return
